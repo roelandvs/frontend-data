733d86fed36ea0686635014d489ecef65c53ca6a
Roeland <roelandsteevan@gmail.com>
2020-11-02 11:55:56 +0100

    extracting specific column is completed

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 
-//endpoint naar SPECIFICATIES PARKEERGEBIED
-const endpoint = "https://opendata.rdw.nl/resource/b3us-f26s.json";
+//endpoint naar SPECIFICATIES PARKEERGEBIED + limit van 2000 items
+const endpoint = "https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000";
+// const endpointTwo = "https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500"; 
 const specificColumn = "areaid";
 
 getData(endpoint) 
+	.then(response => {
+		//returns json() from response
+		return response.json();
+})
 	.then(data => {
 		console.log('all data: ', data);
-		const testjeee = data.map(result => result[specificColumn]);
-		console.log('areaID: ', testjeee);
-	})
+		areaIdArray = filterColumn(data, specificColumn);
+		console.log('areaID: ', areaIdArray);
+})
 
-async function getData(url){
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+//returnt een promise van de url
+function getData(url){
+    return fetch(url);
 };
 
-
-//zonder async
-// function getData(url) {
-// 	return fetch(url); 
-// };
+//returnt 1 specifieke columnNaam
+function filterColumn(RDWArray, columnName) {
+	return RDWArray.map(item => item[columnName])
+};
