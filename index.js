fd4a7d3a798b675c6f6d1d5a4a65931bc4b127bc
Roeland <roelandsteevan@gmail.com>
2020-11-03 13:27:34 +0100

    accepts multiple endpoints in array

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 
 //endpoint naar SPECIFICATIES PARKEERGEBIED + limit van 2000 items
-const endpoint = "https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000";
+// const endpoint = "https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000";
 // const endpointTwo = "https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500"; 
+const endpoints = [
+	getData("https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000"), 
+	getData("https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500")
+];
+
 const specificColumn = "areaid";
 const usefullColumns = ["areaid", "capacity", "disabledaccess"];
 
-getData(endpoint) 
-	.then(response => response.json())
-	.then(RDWData => filterObject(RDWData, usefullColumns))
+Promise.all(endpoints)
+	.then(response => makeJSON(response))
+	.then(RDWData => filterObject(RDWData[0], usefullColumns))
+	// .then waarbij de objecten worden samengevoegd
 	.then(console.log)
 
 //returnt een promise van de url
-function getData(url){
+function getData(url) {
     return fetch(url);
 };
 
-//returnt 1 specifieke columnNaam
-function filterColumn(RDWArray, columnName) {
-	return RDWArray.map(item => item[columnName])
+//makes both endpoint into JSON. This returns a promise again, that's why promise.all is used. 
+function makeJSON(response) {
+	return Promise.all(response.map(response => response.json()));
 };
 
+//returns objects with only the usefull values. It makes objects with only the keys I use
 function filterObject(RDWArray, columnNames) {
-	//returns objects with only the usefull values
 	return RDWArray.map(entry => {
 		let newEntry = {};
 		columnNames.forEach(column => newEntry[column] = entry[column]);
 		return newEntry;
 	})
 };
+
+//function waarbij de objecten samengevoegd worden
