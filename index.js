de5f4f31ecd95f62b5ae3d27a376f41333747912
Roeland <roelandsteevan@gmail.com>
2020-11-03 12:13:26 +0100

    very clean filtering one endpoint

--- index.js
+++ index.js
@@ -3,17 +3,12 @@
 const endpoint = "https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000";
 // const endpointTwo = "https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500"; 
 const specificColumn = "areaid";
+const usefullColumns = ["areaid", "capacity", "disabledaccess"];
 
 getData(endpoint) 
-	.then(response => {
-		//returns json() from response
-		return response.json();
-})
-	.then(data => {
-		console.log('all data: ', data);
-		areaIdArray = filterColumn(data, specificColumn);
-		console.log('areaID: ', areaIdArray);
-})
+	.then(response => response.json())
+	.then(RDWData => filterObject(RDWData, usefullColumns))
+	.then(console.log)
 
 //returnt een promise van de url
 function getData(url){
@@ -23,4 +18,13 @@ function getData(url){
 //returnt 1 specifieke columnNaam
 function filterColumn(RDWArray, columnName) {
 	return RDWArray.map(item => item[columnName])
+};
+
+function filterObject(RDWArray, columnNames) {
+	//returns objects with only the usefull values
+	return RDWArray.map(entry => {
+		let newEntry = {};
+		columnNames.forEach(column => newEntry[column] = entry[column]);
+		return newEntry;
+	})
 };
