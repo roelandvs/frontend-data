50b84705271e5635d49d037b24c81f8b7825df53
Roeland <roelandsteevan@gmail.com>
2020-11-03 15:59:19 +0100

    calling getData function is now done correctly

--- index.js
+++ index.js
@@ -3,25 +3,26 @@
 // const endpoint = "https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000";
 // const endpointTwo = "https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500"; 
 const endpoints = [
-	getData("https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000"), 
-	getData("https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500")
+	"https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000", 
+	"https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500"
 ];
 
 const specificColumn = "areaid";
 const usefullColumns = ["areaid", "capacity", "disabledaccess"];
 
-Promise.all(endpoints)
+getData(endpoints)
 	.then(response => makeJSON(response))
 	.then(RDWData => filterObject(RDWData[0], usefullColumns))
 	// .then waarbij de objecten worden samengevoegd
 	.then(console.log)
 
-//returnt een promise van de url
-function getData(url) {
-    return fetch(url);
+//returnt de url
+function getData(urls) {
+	const RDWDatasets = urls.map(url => fetch(url));
+    return Promise.all(RDWDatasets);
 };
 
-//makes both endpoint into JSON. This returns a promise again, that's why promise.all is used. 
+//makes both endpoint into JSON. This returns a promise again, that's why promise.all is being used. 
 function makeJSON(response) {
 	return Promise.all(response.map(response => response.json()));
 };
@@ -29,9 +30,12 @@ function makeJSON(response) {
 //returns objects with only the usefull values. It makes objects with only the keys I use
 function filterObject(RDWArray, columnNames) {
 	return RDWArray.map(entry => {
+		//Jonah Meijers heeft me geholpen met deze code
+		//-----
 		let newEntry = {};
 		columnNames.forEach(column => newEntry[column] = entry[column]);
 		return newEntry;
+		//-----
 	})
 };
 
