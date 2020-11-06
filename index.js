d17c5af4a44b6e164e0a2b768224b384437366e8
Roeland <roelandsteevan@gmail.com>
2020-11-06 12:24:21 +0100

    filtering geolocation completed

--- index.js
+++ index.js
@@ -4,16 +4,18 @@
 // const endpointTwo = "https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=6500"; 
 const endpoints = [
 	"https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=2000", 
-	"https://opendata.rdw.nl/resource/t5pc-eb34.json"
+	// "https://opendata.rdw.nl/resource/t5pc-eb34.json"
+	"https://opendata.rdw.nl/resource/nsk3-v9n7.json?$limit=7000"
 ];
 
-const usefullColumns = ["areaid", "capacity", "disabledaccess", "areadesc"];
+const usefullColumns = ["areaid", "capacity", "disabledaccess", "areageometryastext", "areamanagerid"];
 
 getData(endpoints)
 	.then(response => makeJSON(response))
 	.then(RDWData => filterEntries(RDWData, usefullColumns))
 	.then(RDWFilteredData => mergeObjects(RDWFilteredData))
-	// .then(console.log)
+	.then(RDWSingleObject => filterGeoLocations(RDWSingleObject))
+	.then(console.log)
 
 //returnt de url met een promise.all 
 function getData(urls) {
@@ -50,18 +52,34 @@ function mergeObjects(dataSet) {
 	let accessDataset = dataSet[0];
 	let locationDataset = dataSet[1];
 
-	accessDataset.map(entry => {
+	return accessDataset.map(entry => {
 		//vergelijkt id's van beide datasets, en als deze overeenkomen bewaard hij ze
-		const locationItem = locationDataset.find(item => item.areaid === entry.areaid);
+		let locationItem = locationDataset.find(item => item.areaid === entry.areaid);
 
 		if (locationItem) {
 			const mergedItem = {...entry, ...locationItem};
-			if (mergedItem.disabledaccess == 1) {
-				console.log(mergedItem);
-			}
+
+			// if (mergedItem.disabledaccess == 1) {
+			// 	console.log('Inclusief disabledaccess: ', mergedItem);
+			// }
+
 			return mergedItem;
 		}
+	}).filter(entry => entry != undefined)
+};
+
+function filterGeoLocations(dataSet) {
+	return dataSet.map(entry => {
+		//RegEx is geschreven door Jonah Meijers
+		const geoLocation = entry.areageometryastext.match(/\d+\.\d+/g);
+		const longitude = geoLocation[0];
+		const latitude = geoLocation[1];
+
+		entry.areageometryastext = [longitude, latitude];
+		return entry;
 	})
 };
 
-//function waarbij de objecten samengevoegd worden
+
+
+
