8c1d3d057f14eb6c35359131fb86563011b4c339
Roeland <roelandsteevan@gmail.com>
2020-11-10 13:03:07 +0100

    geo data merged and ready for d3 use

--- index.js
+++ index.js
@@ -12,18 +12,20 @@ const usefullColumns = [
 	"capacity", 
 	"disabledaccess", 
 	"areageometryastext", 
-	"areamanagerid"
 ];
 
+//geoData is data I saved locally that I reveived from an API
 let geoData = data;
-console.log('geo data:', geoData);
+// console.log('geo data:', geoData);
 
 getData(endpoints)
 	.then(response => makeJSON(response))
 	.then(RDWData => filterEntries(RDWData, usefullColumns))
 	.then(RDWFilteredEntries => mergeObjects(RDWFilteredEntries))
 	.then(RDWSingleObject => filterGeoLocations(RDWSingleObject))
-	.then(RDWFilteredObject => console.log('RDW data: ', RDWFilteredObject))
+	.then(RDWFilteredData => mergeGeoData(RDWFilteredData, geoData))
+	.then(allData => filterAllEntries(allData))
+	.then(console.log)
 
 //returnt de url met een promise.all 
 function getData(urls) {
@@ -56,9 +58,9 @@ function filterEntries(RDWArray, columnNames) {
 	});
 };
 
-function mergeObjects(dataSet) {
-	let accessDataset = dataSet[0];
-	let locationDataset = dataSet[1];
+function mergeObjects(dataset) {
+	let accessDataset = dataset[0];
+	let locationDataset = dataset[1];
 
 	return accessDataset.map(entry => {
 		//vergelijkt id's van beide datasets, en als deze overeenkomen bewaard hij ze
@@ -69,20 +71,52 @@ function mergeObjects(dataSet) {
 			return mergedItem;
 		}
 
-	}).filter(entry => entry != undefined)
+	}).filter(entry => entry != undefined);
 };
 
 function filterGeoLocations(dataset) {
 	return dataset.map(entry => {
 		//RegEx is geschreven door Jonah Meijers
 		const geoLocation = entry.areageometryastext.match(/\d+\.\d+/g);
-		const lng = geoLocation[0];
-		const lat = geoLocation[1];
+		const lng = +geoLocation[0];
+		const lat = +geoLocation[1];
 
-		entry.lng = lng;
-		entry.lat = lat;
+		entry.geo = [lat, lng];
 		return entry;
-	})
+	});
+};
+
+function mergeGeoData(RDWdataset, geoDataset) {
+	return RDWdataset.reduce((acc, cur, i) => {
+   		const mergedItem = {...cur, ...geoDataset[i]};
+   		acc.push(mergedItem);
+   		return acc;
+	}, []);
+};
+
+function filterAllEntries(dataset) {
+	return dataset.map(item => {
+		let city;
+
+		if(item.cityInfo.town) {
+			city = item.cityInfo.town;
+		} else if(item.cityInfo.village) {
+			city = item.cityInfo.village;
+		} else {
+			city = item.cityInfo.city;
+		};
+
+		let cleanObject = {
+			disabledaccess: item.disabledaccess,
+			capacity: item.capacity,
+			city: city,
+			road: item.cityInfo.road,
+			postcode: item.cityInfo.postcode,
+			state: item.cityInfo.state
+		};
+
+		return cleanObject;
+	}).filter(item => item.disabledaccess == 1);
 };
 
 
